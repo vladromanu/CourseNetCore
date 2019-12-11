b8eed87856d016121a2617d7fb1ee2b7f7b932ce
Vlad Spinaru <vlad.spinaru@webbeds.com>
2019-12-12 00:25:09 +0200

    Changes in order

--- Course4-Advanced2/Homework14/Program.cs
+++ Course4-Advanced2/Homework14/Program.cs
@@ -33,6 +33,48 @@ namespace Homework14
 
         }
 
+        private static void SolutionTaskFactoryContinueWhenAll()
+        {
+            int threadsNumber = 10;
+            int batchSize = maxNumber / threadsNumber;
+
+            // Keep the task List and a Record List 
+            Task[] _taskList = new Task[threadsNumber];
+
+            // Create a concurrent collection for adding results
+            ConcurrentBag<Record> bag = new ConcurrentBag<Record>();
+
+            // Create the Task Factory and add all the Tasks from 0 to threadsNumber to process different ranges in parallel 
+            TaskFactory tf = new TaskFactory(TaskCreationOptions.AttachedToParent, TaskContinuationOptions.ExecuteSynchronously);
+
+            for (int i = 0; i < threadsNumber; i++)
+            {
+                // Start new Tasks for each segment we need to calculate
+                _taskList[i] = tf.StartNew(() =>
+                {
+
+                    Record record = Enumerable.Range(i * batchSize, batchSize).Select(number => new Record()
+                    {
+                        Number = number,
+                        DivisorsCount = ProperDivisors(number).Count()
+                    }).OrderByDescending(currentRecord => currentRecord.DivisorsCount).First();
+
+                    bag.Add(record);
+
+                });
+            }
+
+            // Wait for all to finish then output
+            Task.Factory.ContinueWhenAll(_taskList, completedTasks => {
+                foreach (var record in bag)
+                {
+                    Console.WriteLine("{0}: {1}", record.Number, record.DivisorsCount);
+                }
+            });
+
+        }
+
+
 
         private static void SolutionTaskFactoryContinueWithFromParentTask()
         {
@@ -82,46 +124,6 @@ namespace Homework14
             finalTask.Wait();
         }
 
-
-        private static void SolutionTaskFactoryContinueWhenAll()
-        {
-            int threadsNumber = 10;
-            int batchSize = maxNumber / threadsNumber;
-
-            // Keep the task List and a Record List 
-            Task[] _taskList = new Task[threadsNumber];
-
-            // Create a concurrent collection for adding results
-            ConcurrentBag<Record> bag = new ConcurrentBag<Record>();
-
-            // Create the Task Factory and add all the Tasks from 0 to threadsNumber to process different ranges in parallel 
-            TaskFactory tf = new TaskFactory(TaskCreationOptions.AttachedToParent, TaskContinuationOptions.ExecuteSynchronously);
-
-            for (int i = 0; i < threadsNumber; i++)
-            {
-                // Start new Tasks for each segment we need to calculate
-                _taskList[i] = tf.StartNew(() =>
-                {
-
-                    Record record = Enumerable.Range(i * batchSize, batchSize).Select(number => new Record()
-                    {
-                        Number = number,
-                        DivisorsCount = ProperDivisors(number).Count()
-                    }).OrderByDescending(currentRecord => currentRecord.DivisorsCount).First();
-
-                    bag.Add(record);
-
-                });
-            }
-
-            // Wait for all to finish then output
-            Task.Factory.ContinueWhenAll(_taskList, completedTasks => {
-                foreach (var record in bag)
-                {
-                    Console.WriteLine("{0}: {1}", record.Number, record.DivisorsCount);
-                }
-            });
-
-        }
+        
     }
 }
